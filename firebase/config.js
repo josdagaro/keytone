a76a7e1172db9c4c0564022ca9642b7eddab5a6e
José David <josdagaro@gmail.com>
2017-10-20 10:38:09 -0500

    Personal files deleted

--- firebase/config.js
+++
@@ -1,8 +0,0 @@
-var key = require('./key.json');
-
-module.exports = {
-    url: 'https://multi-development.firebaseio.com',
-    credential: function(admin) {
-        return admin.credential.cert(key);
-    }
-}
--- mongo/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'mongodb://decameron:decameron@192.168.209.128:27017/xhprof'
-}
